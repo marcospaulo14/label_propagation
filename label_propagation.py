076f579859c838a5fb24390f0588bbea7005f9f9
guiulhoa <ulhoagui@gmail.com>
2026-08-23 19:05:19 -0300

    funcao que transforma o csv em matriz

--- label_propagation.py
+++ label_propagation.py
@@ -1,5 +1,24 @@
 import random
 
+def read_matrix(nome_arquivo, num_vertices):
+    matrix = [[0] * num_vertices for i in range(num_vertices)]
+    with open(nome_arquivo, 'r') as f:
+        for line in f:
+            i, j = map(int, line.strip().split())
+            matrix[i][j] = 1
+            matrix[j][i] = 1
+    return matrix
+
+#contar o número de vértices em uma matriz de adjacência        
+def count_vertices(nome_arquivo):
+    maior_vertice = -1
+    with open(nome_arquivo, 'r') as f:
+        for line in f:
+            i, j = map(int, line.strip().split(','))
+            maior_vertice = max(maior_vertice, i, j)
+    return maior_vertice + 1
+    
+        
 '''
 def num_vertices(matrix):
     n = len(matrix)
